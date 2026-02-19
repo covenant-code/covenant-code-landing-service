1f6df648b4277453e57def8ee23d805f06de94a5
Heavyve <heavyve@gmail.com>
2026-02-19 13:26:24 +0300

    LBAP-0070 | API_V2 | Landing | Реализация WebSocket

---
+++ src/main/java/ru/covenant/code/landing/ws/dto/WebSocketMessageDto.java
@@ -0,0 +1,32 @@
+package ru.covenant.code.landing.ws.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class WebSocketMessageDto {
+    private String type;
+
+    private Object application;
+
+    private String applicationId;
+
+    private Object stats;
+
+    public WebSocketMessageDto() {
+    }
+
+    public WebSocketMessageDto(String type, Object application) {
+        this.type = type;
+        this.application = application;
+    }
+
+    public WebSocketMessageDto(String type, String applicationId) {
+        this.type = type;
+        this.applicationId = applicationId;
+    }
+
+
+}
