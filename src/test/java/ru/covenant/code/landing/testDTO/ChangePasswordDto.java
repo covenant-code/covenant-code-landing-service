e344a794a61b516f85d808a0bf92082f627c219d
Anna A <markova29@yandex.ru>
2026-02-24 12:17:41 +0300

    LBAP-0071 | API_V2 | Landing | Создание кастомных валидаторов (Часть 1 - 2) #123

---
+++ src/test/java/ru/covenant/code/landing/testDTO/ChangePasswordDto.java
@@ -0,0 +1,31 @@
+package ru.covenant.code.landing.testDTO;
+
+import ru.covenant.code.landing.validation.contraints.PasswordMatch;
+
+@PasswordMatch
+public class ChangePasswordDto {
+    private String newPassword;
+    private String confirmPassword;
+
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+
+    public String getNewPassword() {
+        return newPassword;
+    }
+
+    public void setNewPassword(String newPassword) {
+        this.newPassword = newPassword;
+    }
+
+    public ChangePasswordDto(String newPassword, String confirmPassword) {
+        this.newPassword = newPassword;
+        this.confirmPassword = confirmPassword;
+    }
+}
